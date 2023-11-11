2f6a0ef463acf20b2556f5d984a3798e78c24081
Clayton Ramsey <claytonwramsey@gmail.com>
2023-11-10 20:34:29 -0600

    can actually eager return in forest when in collision

--- pkdt/src/forest.rs
+++ pkdt/src/forest.rs
@@ -112,7 +112,7 @@ impl<const D: usize, const T: usize> PkdForest<D, T> {
 
             not_yet_collided &= radii_squared.simd_lt(dists_sq).cast();
 
-            if (!not_yet_collided).all() {
+            if !not_yet_collided.all() {
                 // all have collided - can return quickly
                 break;
             }
