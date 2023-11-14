93f5c5df25ac4eca4e2704e143abc65a633e11aa
Clayton Ramsey <claytonwramsey@gmail.com>
2023-11-14 14:36:24 -0600

    tweaking my numbers. the reason for slow perf on real data is equal dims

--- pkdt_bench/src/bin/bench.rs
+++ pkdt_bench/src/bin/bench.rs
@@ -8,7 +8,7 @@ use pkdt_bench::{get_points, make_needles};
 use rand::{seq::SliceRandom, Rng, SeedableRng};
 use rand_chacha::ChaCha20Rng;
 
-const N: usize = 1 << 20;
+const N: usize = 1 << 12;
 const L: usize = 16;
 
 fn main() {
