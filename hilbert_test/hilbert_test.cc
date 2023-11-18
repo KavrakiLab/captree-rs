177b2ab350124e1bd6d0f5c0ef2038d133c61854
Zachary Kingston <zak@rice.edu>
2023-11-17 20:23:26 -0600

    filtering with range

--- hilbert_test/hilbert_test.cc
+++ hilbert_test/hilbert_test.cc
@@ -26,7 +26,7 @@ constexpr auto remap_point(float x, float min, float max) -> uint32_t
     return ((x - min) / (max - min)) * static_cast<float>(HilbertFactor);
 }
 
-alignas(32) struct PointHilbert
+struct PointHilbert
 {
     PointHilbert(Point a) : point(a){};
 
@@ -53,82 +53,105 @@ int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        std::cout << "No pointcloud file given. Using randomly generated points." << std::endl;
+        std::cout << "No pointcloud file given." << std::endl;
+        return 0;
     }
-    else
+
+    float filter_range = 1e-2;
+    if (argc > 2)
     {
-        auto fname = std::string(argv[1]);
-        std::cout << "Opening pointcloud file " << fname << "..." << std::endl;
-
-        HighFive::File pointcloud_file(fname, HighFive::File::ReadOnly);
-        auto dataset = pointcloud_file.getDataSet("pointcloud/points");
-        auto raw_pointcloud = dataset.read<std::vector<Point>>();
-        std::cout << raw_pointcloud.size() << " points loaded" << std::endl;
-
-        float min = 0, max = 0;
-        double avg_pair_dist_before = 0;
-        for (auto i = 0u; i < raw_pointcloud.size() - 1; ++i)
-        {
-            avg_pair_dist_before += dist(raw_pointcloud[i], raw_pointcloud[i + 1]);
-
-            min = std::min({min, raw_pointcloud[i][0], raw_pointcloud[i][1], raw_pointcloud[i][2]});
-            max = std::max({max, raw_pointcloud[i][0], raw_pointcloud[i][1], raw_pointcloud[i][2]});
-        }
-        avg_pair_dist_before /= raw_pointcloud.size() - 1;
-
-        std::vector<PointHilbert> hilbert_points(raw_pointcloud.begin(), raw_pointcloud.end());
-
-        auto start_time = std::chrono::steady_clock::now();
-        for (auto i = 0u; i < hilbert_points.size(); ++i)
-        {
-            hilbert_points[i].compute(min, max);
-        }
-
-        pdqsort_branchless(std::begin(hilbert_points), std::end(hilbert_points),
-                           [](const PointHilbert &a, const PointHilbert &b)
-                           { return a.hilbert < b.hilbert; });
-
-        auto sort_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                             std::chrono::steady_clock::now() - start_time)
-                             .count();
-
-        double avg_pair_dist_after = 0;
-        for (auto i = 0u; i < hilbert_points.size() - 1; ++i)
-        {
-            avg_pair_dist_after += dist(hilbert_points[i].point, hilbert_points[i + 1].point);
-        }
-        avg_pair_dist_after /= raw_pointcloud.size() - 1;
-
-        std::cout << "time (s): " << static_cast<double>(sort_time) / 1e9 << std::endl;
-        std::cout << "avg seq-pair dist before: " << avg_pair_dist_before << std::endl;
-        std::cout << "avg seq-pair dist after : " << avg_pair_dist_after << std::endl;
-
-        std::vector<Point> filtered;
-        filtered.resize(raw_pointcloud.size());
-        filtered[0] = hilbert_points[0].point;
-
-        auto j = 0u;
-        for (auto i = 1u; i < hilbert_points.size(); ++i)
-        {
-            const float d = dist(filtered[j], hilbert_points[i].point);
-            if (d > 1e-2)
-            {
-                filtered[++j] = hilbert_points[i].point;
-            }
-        }
-
-        filtered.resize(j);
-        std::cout << "filtered size: " << j << std::endl;
-
-        double avg_pair_dist_filtered = 0;
-        for (auto i = 0u; i < filtered.size() - 1; ++i)
-        {
-            avg_pair_dist_filtered += dist(filtered[i], filtered[i + 1]);
-        }
-        avg_pair_dist_filtered /= raw_pointcloud.size() - 1;
-        std::cout << "avg seq-pair dist filtered: " << avg_pair_dist_filtered << std::endl;
-
-        HighFive::File export_file("filtered.hdf5", HighFive::File::Truncate);
-        export_file.createDataSet("pointcloud/points", filtered);
+        filter_range = stof(argv[2]);
     }
+
+    float max_distance = 1.2;
+    if (argc > 3)
+    {
+        max_distance = stof(argv[2]);
+    }
+
+    std::cout << "Filter range " << filter_range << std::endl;
+
+    auto fname = std::string(argv[1]);
+    std::cout << "Opening pointcloud file " << fname << "..." << std::endl;
+
+    HighFive::File pointcloud_file(fname, HighFive::File::ReadOnly);
+    auto dataset = pointcloud_file.getDataSet("pointcloud/points");
+    auto raw_pointcloud = dataset.read<std::vector<Point>>();
+    std::cout << raw_pointcloud.size() << " points loaded" << std::endl;
+
+    float min = 0, max = 0;
+    double avg_pair_dist_before = 0;
+    for (auto i = 0u; i < raw_pointcloud.size() - 1; ++i)
+    {
+        avg_pair_dist_before += dist(raw_pointcloud[i], raw_pointcloud[i + 1]);
+
+        min = std::min({min, raw_pointcloud[i][0], raw_pointcloud[i][1], raw_pointcloud[i][2]});
+        max = std::max({max, raw_pointcloud[i][0], raw_pointcloud[i][1], raw_pointcloud[i][2]});
+    }
+    avg_pair_dist_before /= raw_pointcloud.size() - 1;
+
+    // std::vector<PointHilbert> hilbert_points(raw_pointcloud.begin(), raw_pointcloud.end());
+    std::vector<PointHilbert> hilbert_points;
+    hilbert_points.reserve(raw_pointcloud.size());
+
+    auto start_time = std::chrono::steady_clock::now();
+
+    for (auto i = 0u; i < raw_pointcloud.size(); ++i)
+    {
+        if (dist(raw_pointcloud[i], {0, 0, 0}) < max_distance)
+        {
+            hilbert_points.emplace_back(raw_pointcloud[i]);
+        }
+    }
+
+    for (auto i = 0u; i < hilbert_points.size(); ++i)
+    {
+        hilbert_points[i].compute(min, max);
+    }
+
+    pdqsort_branchless(std::begin(hilbert_points), std::end(hilbert_points),
+                       [](const PointHilbert &a, const PointHilbert &b) { return a.hilbert < b.hilbert; });
+
+    auto sort_time =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time)
+            .count();
+
+    double avg_pair_dist_after = 0;
+    for (auto i = 0u; i < hilbert_points.size() - 1; ++i)
+    {
+        avg_pair_dist_after += dist(hilbert_points[i].point, hilbert_points[i + 1].point);
+    }
+    avg_pair_dist_after /= raw_pointcloud.size() - 1;
+
+    std::cout << "time (s): " << static_cast<double>(sort_time) / 1e9 << std::endl;
+    std::cout << "avg seq-pair dist before: " << avg_pair_dist_before << std::endl;
+    std::cout << "avg seq-pair dist after : " << avg_pair_dist_after << std::endl;
+
+    std::vector<Point> filtered;
+    filtered.resize(raw_pointcloud.size());
+    filtered[0] = hilbert_points[0].point;
+
+    auto j = 0u;
+    for (auto i = 1u; i < hilbert_points.size(); ++i)
+    {
+        const float d = dist(filtered[j], hilbert_points[i].point);
+        if (d > filter_range)
+        {
+            filtered[++j] = hilbert_points[i].point;
+        }
+    }
+
+    filtered.resize(j);
+    std::cout << "filtered size: " << j << std::endl;
+
+    double avg_pair_dist_filtered = 0;
+    for (auto i = 0u; i < filtered.size() - 1; ++i)
+    {
+        avg_pair_dist_filtered += dist(filtered[i], filtered[i + 1]);
+    }
+    avg_pair_dist_filtered /= raw_pointcloud.size() - 1;
+    std::cout << "avg seq-pair dist filtered: " << avg_pair_dist_filtered << std::endl;
+
+    HighFive::File export_file("filtered.hdf5", HighFive::File::Truncate);
+    export_file.createDataSet("pointcloud/points", filtered);
 }
